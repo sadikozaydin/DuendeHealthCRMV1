454eddf5c0ccabee960f1d19243a7b95b00d7dca
sadikozaydin <sadik@duendedijital.com>
2025-08-05 19:46:11 +0300

    Fix BranchProvider context error

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import './i18n';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { BranchProvider } from './contexts/BranchContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BranchProvider>
+        <App />
+      </BranchProvider>
     </ThemeProvider>
   </StrictMode>
 );
